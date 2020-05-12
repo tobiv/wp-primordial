6519f45f1bc5849e1a41c27af440e378be07b978
Tobias Vogler <tobi@tvdesign.ch>
2020-05-12 11:54:10 +0200

    Convert to object oriented and add settings.

--- wp-primordial.php
+++ wp-primordial.php
@@ -11,43 +11,76 @@
  * Text Domain: wp-primordial
  */
 
-/*
- * Remove jquery embed
- */
-function my_deregister_scripts(){
-   wp_deregister_script( 'wp-embed' );
+class WP_Primordial {
+
+  // Default options
+  private $options = array(
+    'remove_embed'       => true,
+    'remove_emojis'      => true,
+    'remove_generator'   => true,
+    'remove_rsd'         => true,
+    'remove_wlw'         => true,
+    'remove_rest_output' => true,
+    'remove_feed_links'  => true,
+
+    'disable_xmlrpc'     => true,
+
+    'hide_admin_bar'     => true,
+    'hide_core_update'   => true,
+  );
+
+  function __construct() {
+    // Check if config file exists
+    // Read into $options array
+
+    $this->init();
+  }
+
+  private function init() {
+    // Deregister Scripts
+    if($this->options['remove_embed'] === true) {
+      add_action('wp_footer', array('this', 'deregister_scripts'));
+    }
+
+    // Remove idiotic emoji inlines
+    if($this->options['remove_emojis'] === true) {
+      remove_action('wp_head', 'print_emoji_detection_script', 7 );
+      remove_action('wp_print_styles', 'print_emoji_styles' );
+    }
+
+    // Remove stuff from wp_head
+    if($this->options['remove_generator'] === true)   remove_action('wp_head', 'wp_generator');
+    if($this->options['remove_rsd'] === true)         remove_action('wp_head', 'rsd_link');
+    if($this->options['remove_wlw'] === true)         remove_action('wp_head', 'wlwmanifest_link');
+    if($this->options['remove_rest_output'] === true) remove_action('wp_head', 'rest_output_link_wp_head', 10);
+
+    if($this->options['remove_feed_links'] === true) {
+      remove_action('wp_head', 'feed_links_extra', 3);
+      remove_action('wp_head', 'feed_links', 2);
+    }
+
+    // Disable XML-RPC
+    if($this->options['disable_xmlrpc'] === true) {
+      add_filter( 'xmlrpc_enabled', '__return_false' );
+    }
+
+    // Remove admin bar
+    if($this->options['hide_admin_bar'] === true) {
+      show_admin_bar(false);
+    }
+
+    // Hide core update nag in admin
+    if($this->options['hide_core_update'] === true) {
+      remove_action('admin_notices', 'update_nag', 3);
+    }
+  }
+
+  /*
+   * Deregister jquery embed script
+   */
+  private function deregister_scripts() {
+    wp_deregister_script('wp-embed');
+  }
 }
-add_action( 'wp_footer', 'my_deregister_scripts' );
 
-/*
- * Remove idiotic emoji js
- */
-remove_action('wp_head', 'print_emoji_detection_script', 7 );
-remove_action('wp_print_styles', 'print_emoji_styles' );
-
-/*
- * Remove stuff from wp_head
- */
-remove_action('wp_head', 'wp_generator');
-remove_action('wp_head', 'rsd_link');
-remove_action('wp_head', 'wlwmanifest_link');
-remove_action('wp_head', 'rest_output_link_wp_head', 10);
-remove_action('wp_head', 'feed_links_extra', 3);
-remove_action('wp_head', 'feed_links', 2);
-
-/*
- * Remove admin bar and the resulting gap
- */
-add_filter('show_admin_bar', '__return_false');
-function remove_admin_login_header() {
-	remove_action('wp_head', '_admin_bar_bump_cb');
-}
-add_action('get_header', 'remove_admin_login_header');
-
-/*
- * Hide core update nag in admin
- */
-function wphidenag() {
-  remove_action( 'admin_notices', 'update_nag', 3 );
-}
-add_action('admin_menu','wphidenag');
+$tv_wp_primordial = new WP_Primordial();
